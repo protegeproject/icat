29b6d78747df45e8e2be64d962df4f06cb8c7a50
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2012-06-08 20:33:51 +0000

    tagging the trunk to keep track of the axiome work and the swrl work that exists there but nowhere else.

---
+++ src/edu/stanford/bmir/protege/web/client/event/OntologyListenerAdapter.java
@@ -0,0 +1,17 @@
+package edu.stanford.bmir.protege.web.client.event;
+
+public class OntologyListenerAdapter implements OntologyListener {
+
+	public void entityCreated(EntityCreateEvent ontologyEvent) {
+		// TODO Auto-generated method stub
+	}
+
+	public void entityDeleted(EntityDeleteEvent ontologyEvent) {
+		// TODO Auto-generated method stub
+	}
+	
+	public void entityRenamed(EntityRenameEvent renameEvent) {
+		// TODO Auto-generated method stub		
+	}
+
+}
