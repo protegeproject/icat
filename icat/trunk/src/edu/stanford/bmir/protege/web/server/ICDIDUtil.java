acd0dbcb93effc6dcaa13c1ef37ee3ec197c77a9
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2013-03-05 22:33:13 +0000

    when creating a new ICD class, add also the publicId by calling the WHO service

---
+++ icat/trunk/src/edu/stanford/bmir/protege/web/server/ICDIDUtil.java
@@ -0,0 +1,71 @@
+package edu.stanford.bmir.protege.web.server;
+
+import com.google.gson.Gson;
+
+import edu.stanford.smi.protege.util.ApplicationProperties;
+
+
+public class ICDIDUtil {
+
+    private static String WHO_SERVICE_NEW_ID_URL_PROP = "who.service.new.id.url";
+    private static String WHO_SERVICE_NEW_ID_URL_DEFAULT = "http://apps.who.int/classifications/icd11/idgenerator/GetNewId";
+
+    private static String WHO_SERVICE_APIKEY = "who.service.apikey";
+
+    public static String getPublicId(String iCATId) {
+        String url = getWHOServiceNewIdURL() + "?" + "type=entity&apikey=" + getWHOServiceAPIKey() + "&seed=" + URLUtil.encode(iCATId);
+        String jSonText = URLUtil.getURLContent(url);
+
+        if (jSonText == null) { return null;}
+
+        Gson gson = new Gson();
+        ICDPublicId publicId = gson.fromJson(jSonText, ICDPublicId.class);
+        return publicId.getUri();
+    }
+
+    private static String getWHOServiceNewIdURL() {
+        return ApplicationProperties.getString(WHO_SERVICE_NEW_ID_URL_PROP, WHO_SERVICE_NEW_ID_URL_DEFAULT);
+    }
+
+    private static String getWHOServiceAPIKey() {
+        return ApplicationProperties.getString(WHO_SERVICE_APIKEY);
+    }
+
+    public static void main (String[] args) throws Exception{
+        System.out.println(getPublicId("http://who.int/icd#V"));
+    }
+
+
+    /*
+    URL: http://apps.who.int/classifications/icd11/idgenerator/GetNewId
+    Parameters required when calling the service are as follows:
+    type : type of the id to be generated. Only “entity” type is supported at the moment.
+    seed: A seed value to be used to generate the Id.  I guess using  iCat internal ID makes sense here.
+    apiKey : key to be used (for iCat the key is 6507f996-71e9-47af-aa8c-ef4cb6ece3fc) Even though this is not a very high security approach I guess  it’s OK for the time being
+    The result is returned in Json or in Xml depending on your http Accept header
+     */
+    class ICDPublicId {
+
+        private String Uri;
+        private String Type;
+        private Boolean NewlyCreated;
+
+        public ICDPublicId() {
+            //empty constructor
+        }
+
+        public String getUri() {
+            return Uri;
+        }
+
+        public String getType() {
+            return Type;
+        }
+
+        public Boolean getNewlyCreated() {
+            return NewlyCreated;
+        }
+    }
+
+
+}
