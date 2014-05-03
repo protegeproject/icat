f24c470fb4463820b68cc160813021356b6b3391
nyulas <2441046+csnyulas@users.noreply.github.com>
2014-05-03 00:42:58 +0000

    Enforced the implementation of setValues(...) method in the subclasses of AbstractPropertyWidget by declaring it "abstract".

--- src/edu/stanford/bmir/protege/web/client/ui/portlet/propertyForm/HtmlMessageWidget.java
+++ src/edu/stanford/bmir/protege/web/client/ui/portlet/propertyForm/HtmlMessageWidget.java
@@ -1,8 +1,11 @@
 package edu.stanford.bmir.protege.web.client.ui.portlet.propertyForm;
 
+import java.util.Collection;
+
 import com.gwtext.client.widgets.Component;
 
 import edu.stanford.bmir.protege.web.client.model.Project;
+import edu.stanford.bmir.protege.web.client.rpc.data.EntityData;
 import edu.stanford.bmir.protege.web.client.ui.portlet.AbstractPropertyWidget;
 import edu.stanford.bmir.protege.web.client.ui.portlet.html.HtmlTextComponent;
 
@@ -24,6 +27,12 @@ public class HtmlMessageWidget extends AbstractPropertyWidget {
     @Override
     public Component getComponent() {
         return htmlTextComponent;
+    }
+    
+    @Override
+    public void setValues(Collection<EntityData> values) {
+    	// Do nothing.
+    	// This widget is not intended to have values set dynamically    	
     }
 
 }
