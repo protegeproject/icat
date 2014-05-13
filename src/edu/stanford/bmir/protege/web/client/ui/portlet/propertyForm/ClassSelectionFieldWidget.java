31574d0a873f24f6252146472706450f173a5ca6
nyulas <2441046+csnyulas@users.noreply.github.com>
2014-05-13 00:36:19 +0000

    ClassSelectionFieldWidget can have top_class property specified.

--- src/edu/stanford/bmir/protege/web/client/ui/portlet/propertyForm/ClassSelectionFieldWidget.java
+++ src/edu/stanford/bmir/protege/web/client/ui/portlet/propertyForm/ClassSelectionFieldWidget.java
@@ -1,20 +1,32 @@
 package edu.stanford.bmir.protege.web.client.ui.portlet.propertyForm;
 
+import java.util.Map;
+
 import edu.stanford.bmir.protege.web.client.model.Project;
+import edu.stanford.bmir.protege.web.client.rpc.data.PropertyEntityData;
 import edu.stanford.bmir.protege.web.client.ui.ontology.classes.ClassTreePortlet;
 import edu.stanford.bmir.protege.web.client.ui.selection.Selectable;
 
 public class ClassSelectionFieldWidget extends AbstractSelectionFieldWidget {
 
+	private String topClass;
+
 	public ClassSelectionFieldWidget(Project project) {
 		super(project);		
 	}
 
+	@Override
+	public void setup(Map<String, Object> widgetConfiguration,
+			PropertyEntityData propertyEntityData) {
+		super.setup(widgetConfiguration, propertyEntityData);
+        topClass = (String) widgetConfiguration.get(FormConstants.TOP_CLASS);
+	}
 	
 	public Selectable createSelectable() {
 		ClassTreePortlet classTreePortlet = new ClassTreePortlet(getProject());
 		classTreePortlet.setHeight(250);
 		classTreePortlet.setWidth(200);
+		classTreePortlet.setTopClass(topClass);
 		return classTreePortlet;
 	}
 }
