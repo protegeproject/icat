0f9fa4c5a6a2bc1a5ed11ffa711c496afa678b11
Tania Tudorache <tudorache@users.noreply.github.com>
2015-06-03 23:51:28 +0000

    Finalizing the service implementation, and created the upload proposals class.

---
+++ src/edu/stanford/bmir/protege/web/server/icd/proposals/UploadProposalsResponse.java
@@ -0,0 +1,29 @@
+package edu.stanford.bmir.protege.web.server.icd.proposals;
+
+public class UploadProposalsResponse {
+
+	private int httpCode;
+	private String message;
+
+	public UploadProposalsResponse(int httpCode, String message) {
+		this.httpCode = httpCode;
+		this.message = message;
+	}
+	
+	public int getHttpCode() {
+		return httpCode;
+	}
+
+	public void setHttpCode(int httpCode) {
+		this.httpCode = httpCode;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
