7ed83e8cf67aa29d90a4c4334a6661f7541b8288
Tania Tudorache <tudorache@users.noreply.github.com>
2015-07-03 22:32:39 +0000

    made the default max result file 10MB

--- src/edu/stanford/bmir/protege/web/server/icd/proposals/ImportProposalsUtil.java
+++ src/edu/stanford/bmir/protege/web/server/icd/proposals/ImportProposalsUtil.java
@@ -13,8 +13,8 @@ public class ImportProposalsUtil {
 	
 	private static String RESULT_OUTPUT_FILE_PATH_PROP = "icd.proposals.output.path";
 	private static String RESULT_OUTPUT_FILE_PATH_DEFAULT = "icd.proposals.import.result";
-	private static String RESULT_OUTPUT_FILE_MAX_SIZE_PROP = "icd.proposals.output.maxsize";
-	private static int RESULT_OUTPUT_FILE_MAX_SIZE_DEFAULT = 10000;
+	private static String RESULT_OUTPUT_FILE_MAX_SIZE_PROP = "icd.proposals.output.maxsize.in.bytes";
+	private static int RESULT_OUTPUT_FILE_MAX_SIZE_DEFAULT = 10 * 1000000; //10 MB
 
 	public static final String getPropertyName(OWLModel owlModel, String propertyId) {
 		RDFProperty prop = owlModel.getRDFProperty(propertyId);
