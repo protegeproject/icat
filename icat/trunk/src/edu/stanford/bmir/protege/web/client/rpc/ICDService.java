078e9336c17f8106eee4a73b4f66b58877ff7cf5
nyulas <nyulas@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2016-06-04 01:27:24 +0000

    A long due commit of the code in which we separate the more general client-server interaction, which applies for all WHO-FIC classifications, from the ICD specific code. The primary motivation for this separation was to be able to create iCATs for other classifications as well, such as ICHI/ICI.

--- icat/trunk/src/edu/stanford/bmir/protege/web/client/rpc/ICDService.java
+++ icat/trunk/src/edu/stanford/bmir/protege/web/client/rpc/ICDService.java
@@ -1,81 +1,18 @@
 package edu.stanford.bmir.protege.web.client.rpc;
 
 import java.util.Collection;
-import java.util.List;
 
-import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 import edu.stanford.bmir.protege.web.client.rpc.data.EntityData;
-import edu.stanford.bmir.protege.web.client.rpc.data.EntityPropertyValues;
-import edu.stanford.bmir.protege.web.client.rpc.data.SubclassEntityData;
-import edu.stanford.bmir.protege.web.client.rpc.data.icd.AllowedPostcoordinationValuesData;
-import edu.stanford.bmir.protege.web.client.rpc.data.icd.PrecoordinationClassExpressionData;
-import edu.stanford.bmir.protege.web.client.rpc.data.icd.ScaleInfoData;
 
 @RemoteServiceRelativePath("icd")
-public interface ICDService extends RemoteService  {
+public interface ICDService extends WHOFICService  {
 
     public EntityData createICDCls(String projectName, String clsName, Collection<String> superClsNames, String title, String sortingLabel,
             boolean createICDSpecificEntities, String user, String operationDescription, String reasonForChange);
 
-    public List<EntityPropertyValues> getEntityPropertyValuesForLinearization(String projectName, List<String> entities, List<String> properties,
-            List<String> reifiedProps);
-
     public String exportICDBranch(String projectName, String topNode, String userName);
 
-    public List<EntityPropertyValues> getSecondaryAndInheritedTags(String projectName, String clsName);
-
-    public List<SubclassEntityData> getSubclasses(String projectName, String className);
-
-    public EntityPropertyValues changeIndexType(String projectName, String subject, String indexEntity,
-            List<String> reifiedProps, String indexType);
-
-    public EntityPropertyValues changeInclusionFlagForIndex(String projectName, String subject, String indexEntity,
-            List<String> reifiedProps, boolean isInclusionFlag);
-
-    public void removeBaseIndexTerm(String projectName, String entityName,
-            String value, String user, String operationDescription);
-
-    public List<String> getListOfSelectedPostCoordinationAxes(
-    		String projectName, String entity, List<String> reifiedProps);
-
-	public List<EntityPropertyValues> getEntityPropertyValuesForPostCoordinationAxes(
-			String projectName, List<String> entities, List<String> properties,
-			List<String> reifiedProps);
-
-    public boolean addAllowedPostCoordinationAxis(String projectName, String subject,
-   		 	String postcoordinationEntity, String postcoordinationProperty, boolean isRequiredFlag);
-
-    public boolean removeAllowedPostCoordinationAxis(String projectName, String subject,
-    		String postcoordinationEntity, String postcoordinationProperty);
-
-	public List<ScaleInfoData> getPostCoordinationAxesScales(String projectName,
-			List<String> properties);
-
-    public List<EntityData> getAllSuperEntities(String projectName, EntityData entity);
-
-    public List<PrecoordinationClassExpressionData> getPreCoordinationClassExpressions(
-			String projectName, String entity, List<String> properties);
-
-	public List<AllowedPostcoordinationValuesData> getAllowedPostCoordinationValues(
-			String projectName, String entity,
-			List<String> customScaleProperties,
-			List<String> treeValueProperties,
-			List<String> fixedScaleProperties);
-
-	public boolean setPrecoordinationPropertyValue(String projectName, String entity,
-			String property, EntityData oldValue, EntityData newValue);
-
-	public boolean changeIsDefinitionalFlag(String projectName, String entity,
-			String property, boolean isDefinitionalFlag);
-
-    public boolean reorderSiblings(String projectName, String movedClass, String targetClass, boolean isBelow, String parent);
-    
-    public EntityData createInternalReference(String projectName, 
-			EntityData entity, String referenceClassName,
-			String referencePropertyName, String referencedValuePropertyName,
-			EntityData referencedEntity,
-			String user, String operationDescription);
 
 }
---
+++ icat/trunk/src/edu/stanford/bmir/protege/web/client/rpc/WHOFICServiceManager.java
@@ -0,0 +1,137 @@
+package edu.stanford.bmir.protege.web.client.rpc;
+
+import java.util.Collection;
+import java.util.List;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import edu.stanford.bmir.protege.web.client.rpc.data.EntityData;
+import edu.stanford.bmir.protege.web.client.rpc.data.EntityPropertyValues;
+import edu.stanford.bmir.protege.web.client.rpc.data.PropertyEntityData;
+import edu.stanford.bmir.protege.web.client.rpc.data.SubclassEntityData;
+import edu.stanford.bmir.protege.web.client.rpc.data.icd.AllowedPostcoordinationValuesData;
+import edu.stanford.bmir.protege.web.client.rpc.data.icd.PrecoordinationClassExpressionData;
+import edu.stanford.bmir.protege.web.client.rpc.data.icd.ScaleInfoData;
+
+public class WHOFICServiceManager {
+
+    private static ICDServiceAsync proxy;
+    static WHOFICServiceManager instance;
+
+    public static WHOFICServiceManager getInstance() {
+        if (instance == null) {
+            instance = new WHOFICServiceManager();
+        }
+        return instance;
+    }
+
+    private WHOFICServiceManager() {
+        proxy = (ICDServiceAsync) GWT.create(ICDService.class);
+    }
+
+    public void createICDCls(String projectName, String clsName, Collection<String> superClsNames, String title, String sortingLabel,
+               boolean createICDSpecificEntities, String user, String operationDescription, String reasonForChange, AsyncCallback<EntityData> cb) {
+        proxy.createICDCls(projectName, clsName, superClsNames, title, sortingLabel, createICDSpecificEntities, user, operationDescription, reasonForChange, cb);
+    }
+
+    public void getEntityPropertyValuesForLinearization(String projectName, List<String> entities, List<String> properties,
+            List<String> reifiedProps, AsyncCallback<List<EntityPropertyValues>> cb) {
+        proxy.getEntityPropertyValuesForLinearization(projectName, entities, properties, reifiedProps, cb);
+    }
+
+    public void exportICDBranch(String projectName, String topNode, String userName,  AsyncCallback<String> cb){
+        proxy.exportICDBranch(projectName, topNode, userName, cb);
+    }
+
+    public void getSecondaryAndInheritedTags(String projectName, String clsName, AsyncCallback<List<EntityPropertyValues>> cb) {
+        proxy.getSecondaryAndInheritedTags(projectName, clsName, cb);
+    }
+
+    public void getSubclasses(String projectName, String className, AsyncCallback<List<SubclassEntityData>> cb) {
+        proxy.getSubclasses(projectName, className, cb);
+    }
+
+    public void changeIndexType(String projectName, String subject, String indexEntity, List<String> reifiedProps, String indexType, AsyncCallback<EntityPropertyValues> cb) {
+        proxy.changeIndexType(projectName, subject, indexEntity, reifiedProps, indexType, cb);
+    }
+
+    public void changeInclusionFlagForIndex(String projectName, String subject, String indexEntity, List<String> reifiedProps, boolean isInclusionFlag, AsyncCallback<EntityPropertyValues> cb) {
+        proxy.changeInclusionFlagForIndex(projectName, subject, indexEntity, reifiedProps, isInclusionFlag, cb);
+    }
+
+    public void removeBaseIndexTerm(String projectName, String entityName,
+            String value, String user, String operationDescription, AsyncCallback<Void> cb) {
+        proxy.removeBaseIndexTerm(projectName, entityName, value, user, operationDescription, cb);
+    }
+
+    public void getListOfSelectedPostCoordinationAxes(String projectName, String entity,
+    		List<String> reifiedProps, AsyncCallback<List<String>> cb) {
+    	proxy.getListOfSelectedPostCoordinationAxes(projectName, entity, reifiedProps, cb);
+    }
+
+	public void getEntityPropertyValuesForPostCoordinationAxes(String projectName, List<String> entities, List<String> properties,
+			List<String> reifiedProps, AsyncCallback<List<EntityPropertyValues>> cb) {
+		proxy.getEntityPropertyValuesForPostCoordinationAxes(projectName, entities, properties, reifiedProps, cb);
+	}
+
+    public void addAllowedPostCoordinationAxis(String projectName, String subject,
+			String postcoordinationEntity, String postcoordinationProperty,
+			boolean isRequiredFlag, AsyncCallback<Boolean> cb) {
+    	proxy.addAllowedPostCoordinationAxis(projectName, subject,
+    			postcoordinationEntity, postcoordinationProperty, isRequiredFlag, cb);
+    }
+
+    public void removeAllowedPostCoordinationAxis(String projectName, String subject,
+    		String postcoordinationEntity, String postcoordinationProperty, AsyncCallback<Boolean> cb) {
+    	proxy.removeAllowedPostCoordinationAxis(projectName, subject,
+    			postcoordinationEntity, postcoordinationProperty, cb);
+    }
+
+    public void getPostCoordinationAxesScales(String projectName,
+    		List<String> properties, AsyncCallback<List<ScaleInfoData>> cb) {
+    	proxy.getPostCoordinationAxesScales(projectName, properties, cb);
+    }
+
+	public void getAllSuperEntities(String projectName, EntityData entity, AsyncCallback<List<EntityData>> cb) {
+		proxy.getAllSuperEntities(projectName, entity, cb);
+	}
+	
+	public void getPreCoordinationClassExpressions(String projectName, String entity, 
+			List<String> properties, AsyncCallback<List<PrecoordinationClassExpressionData>> cb) {
+		proxy.getPreCoordinationClassExpressions(projectName, entity, properties, cb);
+	}
+	
+	public void getAllowedPostCoordinationValues(String projectName, String entity, 			 
+			List<String> customScaleProperties, List<String> treeValueProperties, List<String> fixedScaleProperties, 
+			AsyncCallback<List<AllowedPostcoordinationValuesData>> cb) {
+		proxy.getAllowedPostCoordinationValues(projectName, entity, 
+				customScaleProperties, treeValueProperties, fixedScaleProperties, cb);
+	}
+	
+	public void setPrecoordinationPropertyValue(String projectName,
+			String entity, String property, 
+			EntityData oldValue, EntityData newValue, AsyncCallback<Boolean> cb) {
+		proxy.setPrecoordinationPropertyValue(projectName, entity, property, oldValue, newValue, cb);
+	}
+
+	public void changeIsDefinitionalFlag(String projectName, String entity,
+			String property, boolean isDefinitionalFlag, AsyncCallback<Boolean> cb) {
+		proxy.changeIsDefinitionalFlag(projectName, entity, property, isDefinitionalFlag, cb);
+	}
+
+	public void reorderSiblings(String projectName, String movedClass, String targetClass, boolean isBelow, String parent,
+            AsyncCallback<Boolean> cb) {
+        proxy.reorderSiblings(projectName, movedClass, targetClass, isBelow, parent, cb);
+    }
+
+	public void createInternalReference(String projectName, EntityData entity,
+			String referenceClassName,
+			String referencePropertyName,
+			String referencedValuePropertyName,
+			EntityData referencedEntity, String user,
+			String operationDescription, AsyncCallback<EntityData> callback) {
+		proxy.createInternalReference(projectName, entity, referenceClassName, referencePropertyName,
+				referencedValuePropertyName, referencedEntity, user, operationDescription, callback);
+	}
+}
