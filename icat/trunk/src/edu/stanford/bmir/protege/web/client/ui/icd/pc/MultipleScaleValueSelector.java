a58889197c4d620a75325e133993521c3ae9c2c3
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2020-11-09 13:20:36 +0000

    removed unfinished class

--- icat/trunk/src/edu/stanford/bmir/protege/web/client/ui/icd/pc/MultipleScaleValueSelector.java
+++
@@ -1,9 +0,0 @@
-package edu.stanford.bmir.protege.web.client.ui.icd.pc;
-
-import java.util.List;
-
-public class MultipleScaleValueSelector {
-	List<AbstractScaleValueSelectorWidget> valueSelWidgets;
-	
-	
-}
