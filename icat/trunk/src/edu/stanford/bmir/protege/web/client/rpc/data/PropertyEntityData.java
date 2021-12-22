b32e380b55b46236c514e89bfedf65b26212971a
nyulas <nyulas@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2021-12-22 06:57:07 +0000

    Moved the handling of indexes in entity names, which was introduced in revision 27495, from EntityData to PropertyEntityData, where it is needed, and also optimized String comparison, in order to improve performance.

--- icat/trunk/src/edu/stanford/bmir/protege/web/client/rpc/data/PropertyEntityData.java
+++ icat/trunk/src/edu/stanford/bmir/protege/web/client/rpc/data/PropertyEntityData.java
@@ -2,25 +2,43 @@ package edu.stanford.bmir.protege.web.client.rpc.data;
 
 import java.util.Collection;
 import java.util.List;
+import com.google.gwt.regexp.shared.RegExp;
 
 public class PropertyEntityData extends EntityData {
+
+	private static final long serialVersionUID = 3996847017712492831L;
 
     private List<EntityData> allowedValues;
     private int minCardinality = 0;
     private int maxCardinality = -1;
     private PropertyType propertyType; //TODO - temporary solution, will be removed
 
+    static RegExp INDEX_REGEX_PATTERN = RegExp.compile("(.*)\\[\\d+\\]$");
 
     public PropertyEntityData() {
-        super(null);
+        this(null, null);
     }
 
     public PropertyEntityData(String name) {
-        super(name);
+        this(name, name);
+    }
+    
+    public PropertyEntityData(String name, String browserText) {
+    	this(name, browserText, null);
     }
 
     public PropertyEntityData(String name, String browserText, Collection<EntityData> types) {
         super(name, browserText, types);
+
+        //TODO: See if we can remove this from here, e.g. by creating a specialized subclass 
+        //		and use that instead of this everywhere(!!!) where we deal with properties coming 
+        //		from the configuration.
+        
+    	//remove index of form [1], [2], etc. from the end of the name (using tests that are optimized for performance)
+    	if (name != null && name.endsWith("]") && INDEX_REGEX_PATTERN.test(name)) {
+    		name = name.substring(0, name.lastIndexOf("[") );
+        	setName(name);
+    	}
     }
 
     public int getMinCardinality() {
